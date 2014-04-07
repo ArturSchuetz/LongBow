616eb5f92f9146862a1024b7d42cb212d3936b47
Artur Schütz <schuetz.artur@yahoo.de>
2014-04-07 23:39:50 +0200

    remove old files

--- Includes/LongBow.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "BowUsedSTL.h"
-#include "BowLogger.h"
-
-typedef std::shared_ptr<void> VoidPtr;
--- Samples/03_FrameBuffer/resource.h
+++
@@ -1,3 +0,0 @@
-#define SHADER						256 // Der Wert ist egal
-#define IDS_VERTEXSHADER            101
-#define IDS_FRAGMENTSHADER          102
