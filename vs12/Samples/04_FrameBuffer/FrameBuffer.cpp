2f2cab9f81904b912374abba91c2221125ea17b9
Artur Schütz <a.schuetz@silverseedgames.com>
2016-09-20 16:54:38 +0200

    removed creation of screenshot in FrameBuffer sample

--- vs12/Samples/04_FrameBuffer/FrameBuffer.cpp
+++ vs12/Samples/04_FrameBuffer/FrameBuffer.cpp
@@ -153,10 +153,6 @@ int main()
 
 		ContextOGL->VSwapBuffers();
 		WindowOGL->VPollWindowEvents();
-
-		std::shared_ptr<void> image = renderTarget->VCopyToSystemMemory(ImageFormat::BlueGreenRed, ImageDatatype::UnsignedByte);
-		Bitmap bmp;
-		bmp.SaveFile(image.get(), WindowOGL->VGetWidth(), WindowOGL->VGetHeight(), "out.bmp");
 	}
 	return 0;
 }
