e555f64fb467835bceba13b704f17603cf2be814
Grigori <grigori.jordan@st.ovgu.de>
2018-03-04 15:34:02 +0100

    For now completed the documentation of quaternions, seems fine.

--- include/BowQuaternion.h
+++ include/BowQuaternion.h
@@ -1,5 +1,4 @@
 #pragma once
-#pragma once
 #include "BowMath.h"
 #include <algorithm>
 
@@ -11,12 +10,19 @@ namespace Bow {
 		{
 		public:
 
+			//! Default constructor of a Quaternion.
+			//! Creates a simple unit Quaternion.
 			Quaternion() : _q(Bow::Core::Vector4<T>(0, 0, 0, 1)) { }
+			//
 			Quaternion(T x, T y, T z, T w) : _q(Bow::Core::Vector4<T>(x, y, z, w)) { }
 
-			// Construct Quaternion from 3D (rotation) matrix.
-			// Matrices here are [row][column].
-			// Taken from 'From Quaterninon to Matrix and Back', J.M.P. van Waveren, February 27th 2005
+			//! Constructor for a Quaternion from a 3D (rotation) matrix.
+			//! Matrices here are [row][column], row major.
+			//! Taken from 'From Quaterninon to Matrix and Back', J.M.P. van Waveren, February 27th 2005
+			/*!
+				\param m Rotation matrix, of which an corresponding quaternion is constructed.
+				\warning The representation of a rotation as a quaternion is not unique! 
+			*/
 			Quaternion(const Bow::Core::Matrix3D<T> &m)
 			{
 				if (m._11 + m._22 + m._33 > 0.0)
@@ -58,6 +64,11 @@ namespace Bow {
 				}
 			}
 
+			//! Elementwise (as it is defined) addition of two quaternions.
+			/*!
+				\param rhs The other quaternion to be added to this quaternion.
+				\return The plus equaled Quaternion.
+			*/
 			Quaternion &operator+=(const Quaternion& rhs)
 			{
 				_q.x += rhs._q.x;
@@ -66,6 +77,12 @@ namespace Bow {
 				_q.w += rhs._q.w;
 				return *this;
 			}
+			//! Elementwise (as it is defined) addition of two quaternions.
+			/*!
+				\param lhs The first quaternion for elementwise quaternion addition.
+				\param rhs The other quaternion to be added elementwise with this quaternion.
+				\return The new quaternion, as a result of the addition.
+			*/
 			friend Quaternion operator+(Quaternion& lhs, const Quaternion& rhs)
 			{
 				Quaternion q;
@@ -76,6 +93,11 @@ namespace Bow {
 				return q;
 			}
 
+			//! Elementwise (as it is defined) subtraction of two quaternions.
+			/*!
+				\param rhs The other quaternion to be subtracted to this quaternion.
+				\return The subtraction equaled Quaternion.
+			*/
 			Quaternion &operator-=(const Quaternion& rhs)
 			{
 				_q.x -= rhs._q.x;
@@ -84,6 +106,12 @@ namespace Bow {
 				_q.w -= rhs._q.w;
 				return *this;
 			}
+			//! Elementwise (as it is defined) subtraction of two quaternions.
+			/*!
+				\param lhs The first quaternion for elementwise quaternion subtraction.
+				\param rhs The other quaternion to be subtracted with this quaternion.
+				\return The new quaternion, as a result of the subtraction.
+			*/
 			friend Quaternion operator-(const Quaternion& lhs, const Quaternion& rhs)
 			{
 				Quaternion q;
@@ -94,6 +122,11 @@ namespace Bow {
 				return q;
 			}
 
+			//!  Elementwise multiplication of the quaternion with a scalar from the right hand side and replacing it 
+			/*!
+				\param rhs Righthand side scalar for the multiplication.
+				\return A new multiplication equaled (*=) quatenion as a result of the scalr multiplication.
+			*/
 			Quaternion &operator*=(const T& rhs)
 			{
 				_q.x *= rhs;
@@ -102,6 +135,12 @@ namespace Bow {
 				_q.w *= rhs;
 				return *this;
 			}
+			//! Elementwise multiplication of the quaternion with a scalar from the left hand side and returning a new resulting quaternion.
+			/*!
+				\param lhs Left hand side scalr for elementwise multiplication.
+				\param rhs Right hand side quaternion for elementwise multiplication
+				\return Resulting quaternion of scalar with quaternion multiplication.
+			*/
 			friend Quaternion operator*(const T& lhs, const Quaternion& rhs)
 			{
 				Quaternion q;
@@ -112,10 +151,31 @@ namespace Bow {
 				return q;
 			}
 
-			// Quaternion multiplication operator
-			// Taken from 'https://www.gamasutra.com/view/feature/131686/rotating_objects_using_quaternions.php?page=2'
-			// Smart rearrangement of quaternion element equations, so parts of an element equation are reused as much as possible, 
-			// whilst keeping arithmetic operations at a minimum.
+			//! Quaternion multiplication operator, with another quaternion and replacing it.
+			/*!
+				\param q2 Right hand side quaternion for quaternion multiplication.
+				\return A new multiplication equaled (*=) quatenion as a result of the quaternion multiplication.
+			
+			* The way quaternion multiplication works:
+			*
+				\f$
+				(qq') = (q_w + q_xi + q_yj + q_zk) + (q'_w + q'_xi + q'_yj + q'_zk)
+				\f$
+			*
+			* Collecting components and using identities of components listed above results in:
+			*
+				\f$
+				(qq')_{xyz} = {\times{q_{xyz}}{q'_{xyz}}} + q_w q'_{xyz} + q'_w q_{xyz}
+				\f$
+			*
+				\f$
+				(qq')_{w} = {\times{q_{w}}{q'_{w}}} + q_w q'_{xyz} \cdot q'_w q_{xyz}
+				\f$
+			*
+			* Taken from 'https://www.gamasutra.com/view/feature/131686/rotating_objects_using_quaternions.php?page=2'.
+			* Smart rearrangement of quaternion element equations, so parts of an element of the equation are reused as much as possible, 
+			* whilst keeping arithmetic operations at a minimum.
+			*/
 			Quaternion &operator*=(const Quaternion& q2)
 			{
 				Quaternion result;
@@ -137,9 +197,32 @@ namespace Bow {
 				return *this;
 			}
 
-			// Quaternion multiplication operator
-			// Taken from 'https://www.gamasutra.com/view/feature/131686/rotating_objects_using_quaternions.php?page=2'
-			// Smart rearrangement of quaternion element equations, so parts of an element equation are reused as much as possible.
+			//! Quaternion multiplication operator, with another quaternion resulting in a new quaternion.
+			/*!
+				\param q1 Left hand side quaternion for elementwise multiplication.
+				\param q2 Right hand side quaternion for elementwise multiplication.
+				\return A new quatenion as a result of the quaternion multiplication.
+
+			* The way quaternion multiplication works:
+			*
+				\f$
+				(qq') = (q_w + q_xi + q_yj + q_zk) + (q'_w + q'_xi + q'_yj + q'_zk)
+				\f$
+			*
+			* Collecting components and using identities of components listed above results in:
+			*
+				\f$
+				(qq')_{xyz} = {\times{q_{xyz}}{q'_{xyz}}} + q_w q'_{xyz} + q'_w q_{xyz}
+				\f$
+			*
+				\f$
+				(qq')_{w} = {\times{q_{w}}{q'_{w}}} + q_w q'_{xyz} \cdot q'_w q_{xyz}
+				\f$
+			*
+			* Taken from 'https://www.gamasutra.com/view/feature/131686/rotating_objects_using_quaternions.php?page=2'.
+			* Smart rearrangement of quaternion element equations, so parts of an element of the equation are reused as much as possible,
+			* whilst keeping arithmetic operations at a minimum.
+			*/
 			friend Quaternion &operator*(const Quaternion& q1, const Quaternion& q2)
 			{
 				Quaternion result;
@@ -161,6 +244,12 @@ namespace Bow {
 				return result;
 			}
 
+			//! Quaternion dot product, same as eucledean vector dot product.
+			/*!
+				\param lhs Left hand side quaternion for dot product computation.
+				\param rhs Right hand side quaternion for dot product computation.
+				\return Dot product of two quaternions.
+			*/
 			inline T Dot(const Quaternion &lhs, const Quaternion &rhs)
 			{
 				//float dotPResult = lhs._imaginary.DotP(rhs._imaginary);
@@ -169,14 +258,23 @@ namespace Bow {
 				return lhs._q.x*rhs._q.x + lhs._q.y*rhs._q.y + lhs._q.z*rhs._q.z + lhs._q.w * rhs._q.w;
 			}
 
+			// Normalize a quaternion, same as with vectors, to the length of one.
+			/*!
+				\param q Quaternion to normalize.
+				\return Normalized quaternion to the length of one. 
+			*/
 			inline Quaternion Normalize(const Quaternion& q)
 			{
 				double length = std::sqrt(Dot(q, q));
 				return (1.0 / std::sqrt(Dot(q, q))) * q;
 			}
 
-			// Matrices here are [row][column].
-			//Taken from 'From Quaterninon to Matrix and Back', J.M.P. van Waveren, February 27th 2005
+			//! Matrices here are [row][column], row major.
+			/*!
+				\return Corresponding 3D rotation matrix in xyz space.
+				\warning The representation of a rotation as a quaternion is not unique! 
+			* Taken from 'From Quaterninon to Matrix and Back', J.M.P. van Waveren, February 27th 2005
+			*/
 			Bow::Core::Matrix3D<T> To3DMatrix() const
 			{
 				Bow::Core::Matrix3D<T> m;
@@ -216,13 +314,23 @@ namespace Bow {
 				return m;
 			}
 
+			//! Spherical interpolation between two quaternions, with parameter t.
+			/*!
+				\param t Interpolation paramter. Should range from zero to one. 
+				\param q1 Start quaternion of the spherical interpolation.
+				\param q2 Target quaternion of the spherical interpolation.
+				\return Spherical interpolated quaternion.
+				\warning The representation of a rotation as a quaternion is not unique! 
+			*/
 			Quaternion Slerp(float t, const Quaternion &q1, const Quaternion &q2)
 			{
 				float cosTheta = Dot(q1, q2);
+				// If target and destination quaternion distance is too small use direct interpolation.
 				if (cosTheta > 0.9995)
 				{
 					return (1 - t) * q1 + t * q2;
 				}
+				// Else use spherical interpolation.
 				else
 				{
 					float theta = std::acos(std::max<float>(std::min<float>(cosTheta, 1.0), -1.0));
@@ -240,7 +348,7 @@ namespace Bow {
 			T W() { return _q.w; }
 
 		private:
-			Bow::Core::Vector4<T> _q;
+			Bow::Core::Vector4<T> _q;	/// Four element vector holding x,y,z and w of the quaternion.
 			//Bow::Core::Vector3<float> _imaginary;
 			//float _real;
 		};
