2bc0ecbd03b3b3592ce1ec06acda5bad5f639043
Artur Schütz <art.schuetz@gmail.com>
2018-03-22 02:42:17 +0100

    fixed x64 issues

--- src/Engine/RenderDeviceImplementations/DirectX12RenderDevice/BowDirectX12RenderDevice.cpp
+++ src/Engine/RenderDeviceImplementations/DirectX12RenderDevice/BowDirectX12RenderDevice.cpp
@@ -35,22 +35,6 @@ namespace bow {
 
 	bool DirectX12RenderDevice::Initialize(void)
 	{
-        UINT dxgiFactoryFlags = 0;
-
-#if defined(_DEBUG)
-		// Enable the debug layer (requires the Graphics Tools "optional feature").
-		// NOTE: Enabling the debug layer after device creation will invalidate the active device.
-		{
-			ComPtr<ID3D12Debug> debugController;
-			if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
-			{
-				debugController->EnableDebugLayer();
-
-				// Enable additional debug layers.
-				dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
-			}
-		}
-#endif
 
         return true;
 	}
